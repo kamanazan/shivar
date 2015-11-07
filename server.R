76e7a6f6370f99c1d1de3f905d55bc13ec786a32
Muhammad F. Rosyadi <muhammad.fauzan@eezee-it.com>
2015-11-07 17:45:45 +0700

    code clean-up

--- server.R
+++ server.R
@@ -1,55 +1,49 @@
 library(shiny)
 library(ggplot2)
-#library(data.table)
-#library(scales)
-#library(stockPortfolio)
-#library(plyr)
-#library(RQuantLib)
-#library(quantmod)
-#library(e1071)
-#library(gridExtra)
-#library(lubridate)
-#library(magrittr)
 library(vars)
 
 #data_sumber <- read.csv("canada.csv")
 
-shinyServer(
-  function(input, output) {
+shinyServer(function(input, output) {
+  data_sumber <- reactive({
+    if (length(input$sumber_data$name) > 0){
+      dt <-read.csv(input$sumber_data$datapath)
+    }
+    else
+      dt <- list()
+    return(dt)
+  })
+  
+  output$pilih_kolom <- renderUI({
+    kolom <- colnames(data_sumber())
+    selectInput(
+      "var_column",
+      label = "Kolom yang diteliti",
+      choices = kolom,
+      selected = kolom[2]
+    )
+  })
+  
+  var_process <- reactive({
+    vr <-
+      VARselect(data_sumber(),lag.max = input$lag_max, type = input$var_type)
     
-    data_sumber <- reactive({
-      read.csv(input$sumber_data$datapath)
-    })
+  })
+  
+  output$var_select <- renderTable({
+    vselect <- var_process()
+    vselect$criteria
+  })
+  
+  output$data_table <- renderTable({
+    data_sumber()
+  })
+  
+  output$var_result <- renderPlot({
+    vselect <- var_process()
     
-    output$pilih_kolom <- renderUI({
-      kolom <- colnames(data_sumber())
-      selectInput("var_column", 
-                  label = "Kolom yang diteliti",
-                  choices = kolom,
-                  selected = kolom[2])
-    })
-    
-    var_process <- reactive({
-     
-        vr <- VARselect(data_sumber(),lag.max=input$lag_max, type=input$var_type)
-      
-    })
-    
-    output$var_select <- renderTable({ 
-      vselect <- var_process()
-      vselect$criteria
-    })
-    
-    output$data_table <- renderTable({
-      data_sumber()
-    })
-    
-    output$var_result <- renderPlot({
-      vselect <- var_process()
-      
-      p_val <- vselect$selection[['SC(n)']]
-      p1ct <- VAR(data_sumber(), p=p_val, type=input$var_type)
-      plot(p1ct, names=input$var_column)
-    })
-  }
-)
+    p_val <- vselect$selection[['SC(n)']]
+    p1ct <- VAR(data_sumber(), p = p_val, type = input$var_type)
+    plot(p1ct, names = input$var_column)
+  })
+})
--- ui.R
+++ ui.R
@@ -1,79 +1,81 @@
 shinyUI(fluidPage(
-  
   #withMathJax(), # to display LaTeX-like formulas
   
-  tags$head( # CSS insert
-    tags$style(HTML("      
-                    td, th, tr {
-                    padding-right: 10px;
-                    padding-left: 10px;
-                    border: 1px solid #d3d3d3;
-                    }
-                    border-color {
-                    #d3d3d3
-                    }
-                    "))
-    ),
+  tags$head(# CSS insert
+    tags$style(
+      HTML(
+        "
+        td, th, tr {
+        padding-right: 10px;
+        padding-left: 10px;
+        border: 1px solid #d3d3d3;
+        }
+        border-color {
+        #d3d3d3
+        }
+        "
+      )
+      )),
   
   titlePanel("VAR analysis"),
   
   br(),
   
-  sidebarLayout(sidebarPanel(
+  sidebarLayout(
+    sidebarPanel(
+      #textInput("ticker", h3("Security ticker"), value="SPY"),
+      
+      h3("Pengaturan VAR"),
+      
+      fileInput("sumber_data", label = "pilih data"),
+      
+      selectInput(
+        "var_type",
+        label = "Pilih Tipe Deterministik",
+        choices = c("both", "const",
+                    "trend","none"),
+        selected = "both"
+      ),
+      
+      sliderInput(
+        "lag_max", h4("Interval Lag"),
+        min = 1,
+        max = 10,
+        value = 1
+      ),
+      
+      # Pemilihan variable yang akan diteliti
+      # TODO:
+      #1. buat choices dinamis tergantung jenis data
+      #   yang digunakan
+      #2. Opsi 'semua' untuk menampilkan grafik semua kolom
+      uiOutput('pilih_kolom'),
+      
+      width = 3
+    ),
     
-    #textInput("ticker", h3("Security ticker"), value="SPY"),
-    
-    h3("Pengaturan VAR"),
-    
-    fileInput("sumber_data", label="pilih data"),
-    
-    selectInput("var_type", 
-                label = "Pilih Tipe Deterministik",
-                choices = c("both", "const",
-                            "trend","none"),
-                selected = "both"),
-    
-    sliderInput("lag_max", h4("Interval Lag"),
-                min = 1,
-                max = 10,
-                value = 1
-                ),
-    
-    # Pemilihan variable yang akan diteliti
-    # TODO: 
-    #1. buat choices dinamis tergantung jenis data
-    #   yang digunakan
-    #2. Opsi 'semua' untuk menampilkan grafik semua kolom
-    uiOutput('pilih_kolom'),
-    
-    width = 3
-  ),
-  
-  mainPanel(
-    
-    tabsetPanel(position=c("right"),
-                tabPanel(strong("Plots"), 
-                         br(),
-                         plotOutput("var_result"),
-                         #plotOutput("scatterplot"),
-                         #htmlOutput("metrics_stats"),
-                         #htmlOutput("metrics_finance"),
-                         br()),
-                
-                tabPanel(strong("Data"), 
-                         br(),
-                         tableOutput("var_select"),
-                         p("..."),
-                         br(),
-                         #tableOutput("table_tail"),
-                         #code("Displays no more than 200 rows."),
-                         br()),
-                
-                tabPanel(strong("Annualized data"),
-                         br(),
-                         tableOutput("data_table"),
-                         br())
-                
-                )
-  ))
+    mainPanel(tabsetPanel(
+      position = c("right"),
+      tabPanel(strong("Plots"),
+               br(),
+               plotOutput("var_result"),
+               br()),
+      
+      tabPanel(
+        strong("Data"),
+        br(),
+        tableOutput("var_select"),
+        br(),
+        br()
+      ),
+      
+      tabPanel(
+        strong("Annualized data"),
+        br(),
+        tableOutput("data_table"),
+        br()
+      )
+      
     ))
+  )
+      ))
